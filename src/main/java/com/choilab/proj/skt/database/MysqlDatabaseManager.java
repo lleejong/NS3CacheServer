dccffeadb31b06559c7bc3b12cdf5dd232bf3418
lleejong <lleejong@gmail.com>
2016-12-16 19:49:46 +0900

    query form 변경

--- src/main/java/com/choilab/proj/skt/database/MysqlDatabaseManager.java
+++ src/main/java/com/choilab/proj/skt/database/MysqlDatabaseManager.java
@@ -36,7 +36,7 @@ public class MysqlDatabaseManager implements DatabaseManager {
 		// this.connectionUrl = "jdbc:mysql://" + ip + ":" + port + "/" +
 		// dbName;
 		this.connectionUrl = "jdbc:mysql://" + ip + "/" + dbName;
-		
+
 	}
 
 	public void init() {
@@ -57,24 +57,31 @@ public class MysqlDatabaseManager implements DatabaseManager {
 
 		return conn;
 	}
-	
-	public NS3Data cacheQuery(NS3Data data){
-		String query = "SELECT B.* FROM (SELECT A.*, ABS(? - A.TotalDelay)" + " + ABS(? - A.TotalJitter) + ABS(? - A.TxLoss) + " + "ABS(? - A.RxLoss) AS EuclideanDistance FROM ns3data A) B WHERE ? "
-				+ "> B.TotalDelay AND ? > B.TotalJitter AND ? > B.TxLoss AND ? > B.RxLoss ORDER BY B.EuclideanDistance LIMIT 1; ";
+
+	public NS3Data cacheQuery(NS3Data data) {
+
+		// SELECT B.* FROM (SELECT A.*, SQRT(POW('1500' - A.TotalDelay, 2) +
+		// POW('100' - A.TotalJitter, 2) + POW('0' - A.TxLoss, 2) + POW('0' -
+		// A.RxLoss, 2)) AS EuclideanDistance FROM ns3data A) B WHERE '1500' >
+		// B.TotalDelay AND '100' > B.TotalJitter ORDER BY B.EuclideanDistance
+		// LIMIT 1;
 		
-		try{
-			
+		String query = "SELECT B.* FROM (SELECT A.*, ABS(? - A.TotalDelay) + ABS(? - A.TotalJitter) + ABS(? - A.TxLoss) + ABS(? - A.RxLoss) AS EuclideanDistance FROM ns3data A) B WHERE ? "
+				+ " > B.TotalDelay AND ? > B.TotalJitter AND ? > B.TxLoss AND ? > B.RxLoss ORDER BY B.EuclideanDistance LIMIT 1; ";
+
+		try {
+
 			Connection conn = getConnection();
-			
+
 			PreparedStatement pstmt = conn.prepareStatement(query);
-			pstmt.setDouble(1, data.getTxDelay() + data.getRxDelay());
-			pstmt.setDouble(5, data.getTxDelay() + data.getRxDelay());
-			pstmt.setDouble(2, data.getTxJitter() + data.getRxJitter());
-			pstmt.setDouble(6, data.getTxJitter() + data.getRxJitter());
-			pstmt.setDouble(3, data.getTxLoss());
-			pstmt.setDouble(7, data.getTxLoss());
-			pstmt.setDouble(4, data.getRxLoss());
-			pstmt.setDouble(8, data.getRxLoss());
+			pstmt.setString(1, "'"+data.getTxDelay() + data.getRxDelay()+"'");
+			pstmt.setString(5, "'"+data.getTxDelay() + data.getRxDelay() + "'");
+			pstmt.setString(2, "'"+data.getTxJitter() + data.getRxJitter()+"'");
+			pstmt.setString(6, "'"+data.getTxJitter() + data.getRxJitter()+"'");
+			pstmt.setString(3, "'"+data.getTxLoss()+"'");
+			pstmt.setString(7, "'"+data.getTxLoss()+"'");
+			pstmt.setString(4, "'"+data.getRxLoss()+"'");
+			pstmt.setString(8, "'"+data.getRxLoss()+"'");
 			ResultSet rs = pstmt.executeQuery(query);
 			double txLoss = rs.getDouble("TxLoss");
 			double txDelay = rs.getDouble("TxDelay");
@@ -86,14 +93,13 @@ public class MysqlDatabaseManager implements DatabaseManager {
 			double throughput = rs.getDouble("Throughput");
 
 			return new NS3Data(txLoss, txDelay, txJitter, rxLoss, rxDelay, rxJitter, throughput);
-			
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
-		
+
 	}
 
 	public List<NS3Data> fetchData() {
@@ -111,16 +117,18 @@ public class MysqlDatabaseManager implements DatabaseManager {
 			// '1500' > B.TotalDelay AND '100' > B.TotalJitter ORDER BY
 			// B.EuclideanDistance LIMIT 1;
 
-//			String query = "SELECT B.* FROM (SELECT A.*, ABS(? - A.TotalDelay)"
-//					+ " + ABS(? - A.TotalJitter) + ABS(? - A.TxLoss) + "
-//					+ "ABS(? - A.RxLoss) AS EuclideanDistance FROM ns3data A) B WHERE ? "
-//					+ "> B.TotalDelay AND ? > B.TotalJitter AND ? > B.TxLoss AND ? > B.RxLoss ORDER BY B.EuclideanDistance LIMIT 1; ";
-			
+			// String query = "SELECT B.* FROM (SELECT A.*, ABS(? -
+			// A.TotalDelay)"
+			// + " + ABS(? - A.TotalJitter) + ABS(? - A.TxLoss) + "
+			// + "ABS(? - A.RxLoss) AS EuclideanDistance FROM ns3data A) B WHERE
+			// ? "
+			// + "> B.TotalDelay AND ? > B.TotalJitter AND ? > B.TxLoss AND ? >
+			// B.RxLoss ORDER BY B.EuclideanDistance LIMIT 1; ";
+
 			String query = "SELECT * FROM ns3data";
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 
-			
 			// double txLoss, double txDelay, double txJitter, double rxLoss,
 			// double rxDelay, double rxJitter
 			// TxDelay, TxLoss, TxJitter, RxDelay, RxLoss, RxJitter, Throughput
