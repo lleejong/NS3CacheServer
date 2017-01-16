b10cbe47ba0c0a1a89e8d72f51cc1ae4873bf206
lleejong <lleejong@gmail.com>
2017-01-16 15:35:53 +0900

    패키지명 변경

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 args=("$@");
-java -cp ./target/NS3CacheServer-0.0.1-SNAPSHOT.jar:/root/.m2/repository/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar com.choilab.proj.skt.App ${args[0]} ${args[1]} ${args[2]} 
+java -cp ./target/NS3CacheServer-0.0.1-SNAPSHOT.jar:/root/.m2/repository/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar proj.skt.App ${args[0]} ${args[1]} ${args[2]} 
